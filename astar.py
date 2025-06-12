588bdf42f29c89a4c9d4bdfa15db11f9f4a98d99
usman <usman.ghani4501@gmail.com>
2025-06-12 10:43:48 +0200

    Refactored code into separate files

--- astar.py
+++ astar.py
@@ -1,104 +1,12 @@
 import pygame
-import math
 from queue import PriorityQueue
+
+from spot import Spot
+from constants import WHITE, GREY
 
 WIDTH = 800
 WIN = pygame.display.set_mode((WIDTH, WIDTH))  # setting display
 pygame.display.set_caption("A* Path Finding Visualiser")
-
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 255, 0)
-YELLOW = (255, 255, 0)
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-PURPLE = (128, 0, 128)
-ORANGE = (255, 165, 0)
-GREY = (128, 128, 128)
-TURQUOISE = (64, 224, 208)
-
-
-class Spot:
-    def __init__(self, row: int, col: int, width: int, total_rows: int):
-        self.row = row
-        self.col = col
-
-        # coordinates of a spot on grid
-        self.x = row * width
-        self.y = col * width
-
-        self.color = WHITE
-        self.neighbors = []
-        self.width = width
-        self.total_rows = total_rows
-
-    def get_pos(self):
-        return self.row, self.col
-
-    def is_closed(self):
-        return self.color == RED
-
-    def is_open(self):
-        return self.color == GREEN
-
-    def is_barrier(self):
-        return self.color == BLACK
-
-    def is_start(self):
-        return self.color == ORANGE
-
-    def is_end(self):
-        return self.color == TURQUOISE
-
-    def reset(self):
-        self.color = WHITE
-
-    def make_start(self):
-        self.color = ORANGE
-
-    def make_closed(self):
-        self.color = RED
-
-    def make_open(self):
-        self.color = GREEN
-
-    def make_barrier(self):
-        self.color = BLACK
-
-    def make_end(self):
-        self.color = TURQUOISE
-
-    def make_path(self):
-        self.color = PURPLE
-
-    def draw(self, win):
-        pygame.draw.rect(
-            win, self.color, (self.x, self.y, self.width, self.width))
-
-    def update_neighbors(self, grid):
-        """
-            Check up, down, left and right to check if those are barriers or not. If they are not barriers, we will add them to the neighbors list
-        """
-        self. neighbors = []
-        # moving down a row (ie. self.row + 1)
-        if self.row < self.total_rows - 1 and not grid[self.row + 1][self.col].is_barrier():
-            self.neighbors.append(grid[self.row + 1][self.col])
-
-        # moving up a row (ie. self.row - 1)
-        if self.row > 0 and not grid[self.row - 1][self.col].is_barrier():
-            self.neighbors.append(grid[self.row - 1][self.col])
-
-        # checking right spot (ie. self.col + 1)
-        if self.col < self.total_rows - 1 and not grid[self.row][self.col + 1].is_barrier():
-            self.neighbors.append(grid[self.row][self.col + 1])
-
-        # checking left spot (ie. self.col - 1)
-        if self.col > 0 and not grid[self.row][self.col - 1].is_barrier():
-            self.neighbors.append(grid[self.row][self.col - 1])
-
-    def __lt__(self, other):
-        return False
-
 
 def h(p1, p2):
     # Calculate distance between point 1 and point 2 using manhatten distance
